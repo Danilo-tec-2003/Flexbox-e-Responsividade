baabb40a87a9b42c46dcd37f6efaff9ee4f2de8f
Danilo Mendes <danilodev.br@gmail.com>
2024-08-16 19:57:54 -0300

    MeuEstudo

--- App.tsx
+++ App.tsx
@@ -1,10 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+function CaixaRed() {
+  return <View
+    style={{
+      height: 300,
+      width: 100,
+      backgroundColor: 'red',
+     
+    }}>
+  </View>
+}
+
+function CaixaBlue() {
+  return <View
+    style={{
+      height: 300,
+      width: 100,
+      backgroundColor: 'blue',
+      
+      
+    }}>
+  </View>
+}
+
+function CaixaGreen() {
+  return <View
+    style={{
+      height: 100,
+      width: 100,
+      backgroundColor: 'green',
+      flexShrink: 1,
+      borderWidth: 5,
+    borderColor: 'black'
+     
+    }}>
+  </View>
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <CaixaRed/>
+      <CaixaBlue/>
+      <CaixaGreen/>
+     
+     
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +54,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    borderWidth: 5,
+    borderColor: 'purple',
+    backgroundColor: 'white',
+    
   },
 });
+
